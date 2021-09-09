ad5fcc410ef62f767985246d319b71ef85618f22
muroni-cristiano <cristiano.muroni@compasso.com.br>
2021-09-09 00:22:59 -0300

    corrigindo relacionamentos no model projects

--- api/models/ProjectsModel.js
+++ api/models/ProjectsModel.js
@@ -4,6 +4,7 @@ const OwnersPlacesModel = require("./OwnersPlacesModel");
 const AddressModel = require("./AddressModel");
 const CustomersModel = require("./CustomersModel");
 const UsersModel = require("./UsersModel");
+const OperatorsModel = require("./OperatorsModel");
 
 
 const ProjectsModel = connection.define("Projects", {
@@ -44,21 +45,31 @@ const ProjectsModel = connection.define("Projects", {
 });
 
 // Projects table relationships
+AddressModel.hasOne(ProjectsModel);
 ProjectsModel.belongsTo(AddressModel, {
   foreignKey: {
     name: "place",
   },
 });
+
+CustomersModel.hasOne(ProjectsModel);
 ProjectsModel.belongsTo(CustomersModel, {
   foreignKey: {
     name: "project_author",
   },
 });
+
+OwnersPlacesModel.hasOne(ProjectsModel);
 ProjectsModel.belongsTo(OwnersPlacesModel);
 
 // Customers table relationships
+UsersModel.hasOne(CustomersModel);
 CustomersModel.belongsTo(UsersModel);
+
+AddressModel.hasOne(CustomersModel);
 CustomersModel.belongsTo(AddressModel);
+
+ProjectsModel.hasOne(CustomersModel);
 CustomersModel.hasMany(ProjectsModel, {
   foreignKey: {
     name: "project_author",
@@ -66,11 +77,17 @@ CustomersModel.hasMany(ProjectsModel, {
 });
 
 // OwnersPlaces table relationships
+UsersModel.hasOne(OwnersPlacesModel);
 OwnersPlacesModel.belongsTo(UsersModel);
+
+AddressModel.hasOne(OwnersPlacesModel);
 OwnersPlacesModel.belongsTo(AddressModel);
+
+ProjectsModel.hasOne(OwnersPlacesModel);
 OwnersPlacesModel.hasMany(ProjectsModel);
 
 // Operators table relationships
+UsersModel.hasOne(OperatorsModel);
 OperatorsModel.belongsTo(UsersModel);
 
 ProjectsModel.sync({force: true});
