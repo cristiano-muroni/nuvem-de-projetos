ec0c29a6aed278485ca1c0ebfca8fb4a0bc881fb
Cristiano Muroni <muronislick@hotmail.com>
2021-11-30 22:38:06 -0300

    criando a viwe de customer e a rota render de customer

--- api/controllers/customerController.js
+++ api/controllers/customerController.js
@@ -2,6 +2,11 @@ const list = (req,res) => {
     res.send('you are on the customer route')
 };
 
+const createViewCustomer = (req, res) => {
+    res.render("customers/createCustomers");
+  };
+
 module.exports ={
-    list
+    list,
+    createViewCustomer
 };
--- api/routes/customerRoutes.js
+++ api/routes/customerRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 
 router.get('/customerList', customerController.list );
+router.get('/createViewCustomer', customerController.createViewCustomer);
 
 module.exports = router;
